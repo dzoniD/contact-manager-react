2a8304154c6726df437f119fc09eaa893c8a5ee6
dzoniD <edvard_dantes@hotmail.com>
2019-06-09 17:32:33 +0200

    put request and update action

--- src/components/contacts/EditContact.js
+++ src/components/contacts/EditContact.js
@@ -53,6 +53,18 @@ class EditContact extends Component {
       return;
     }
 
+    const updContact = {
+      name,
+      email,
+      phone
+    }
+
+    const { id } = this.props.match.params;
+    
+    const res = await axios.put(`https://jsonplaceholder.typicode.com/users/${id}`,updContact);
+
+    dispatch({type:'UPDATE_CONTACT',payload:res.data});
+
     // CLear state
     this.setState({
       name: "",
