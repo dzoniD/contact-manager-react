a25fad2b0553ea9cd339dab555ea5f7d317d90e8
dzoniD <edvard_dantes@hotmail.com>
2019-06-09 17:56:15 +0200

    Updates

---
+++ precache-manifest.06f6aadb53a0acd2d0f121c32f803ac3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61f2aee5d5aed3912c2edd6aab68bd7d",
+    "url": "/contact-manager-react/index.html"
+  },
+  {
+    "revision": "3b1b3bff2b7e101b830a",
+    "url": "/contact-manager-react/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "5feec660192b6233c587",
+    "url": "/contact-manager-react/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "3b1b3bff2b7e101b830a",
+    "url": "/contact-manager-react/static/js/2.292b09d7.chunk.js"
+  },
+  {
+    "revision": "5feec660192b6233c587",
+    "url": "/contact-manager-react/static/js/main.21e24bfb.chunk.js"
+  },
+  {
+    "revision": "b669651a203277be328c",
+    "url": "/contact-manager-react/static/js/runtime~main.3ce19021.js"
+  }
+]);
